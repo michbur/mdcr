74c917577f952d62fb3af46d838558a140be65d4
michbur <michalburdukiewicz@gmail.com>
2015-11-18 07:42:36 +0100

    changes for discussion with SR

--- R/coRpus.R
+++ R/coRpus.R
@@ -14,6 +14,17 @@ coRpus <- function(source = NULL) {
   # create a corpus source from as DataframeSource from the textinput data
   corpus.source <- DataframeSource(data.frame(text = source))
   corpus <- Corpus(corpus.source)
+  
+  # Remove white spaces
+  corpus <- tm_map(corpus, stripWhitespace)
+  
+  # Remove stopwords (optionally give language as an parameter)
+  # TO DO remove also words like study or research
+  corpus <- tm_map(corpus, removeWords, stopwords("english"))
+  
+  # Stemming
+  corpus <- tm_map(corpus, stemDocument)
+  
   # Create document term matrix
   corpus.DTM <- DocumentTermMatrix(corpus, control = list(weighting = weightTf, stopwords = TRUE))
   corpus.DTM
